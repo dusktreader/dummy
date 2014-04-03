3b26a2b6bc5a63498ed251a6ac03020595d2c823
hn269 <hn269@cornell.edu>
2014-04-03 12:18:07 -0400

    change build folder

--- build/cheesecake/tests/data/import_self.py
+++
@@ -1,9 +0,0 @@
-"""This module will get W0406 warning for importing self,
-which should be ignored in Cheesecake score.
-"""
-
-__revision__ = 'satisfy pylint checker'
-
-import import_self
-
-print import_self.__revision__ # use imported module
--- build/cheesecake/tests/unit/_path_cheesecake.py
+++
@@ -1,4 +0,0 @@
-import sys, os
-testdir = os.path.dirname(__file__)
-sys.path.insert(0, os.path.join(testdir, '../../'))
-
