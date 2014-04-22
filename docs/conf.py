d67b4adc9c5a0561d27916c9af74220af080107e
hn269 <hn269@cornell.edu>
2014-04-21 20:57:58 -0400

    icons

--- docs/conf.py
+++ docs/conf.py
@@ -59,15 +59,15 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'dummy'
-copyright = u'2014, Author'
+project = u'Dummy'
+copyright = u'2014, Hoang Long Nguyen'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = ''
+version = '0.1.0'
 # The full version, including alpha/beta/rc tags.
 release = ''
 
