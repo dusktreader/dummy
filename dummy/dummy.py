dca3be4f4211e6039eb0fbae8b470c0ed23d54a6
hn269 <hn269@cornell.edu>
2014-04-21 20:34:13 -0400

    icons

--- dummy/dummy.py
+++ dummy/dummy.py
@@ -9,7 +9,7 @@
 The Python Documentation and Release Project
 ============================================
 
-.. image:: https://raw.githubusercontent.com/hn269/dummy/develop/icons/Dummy_icon.png
+.. image:: icons\Dummy_icon.png
     :height: 100px
     :alt: logo
     :align: center
